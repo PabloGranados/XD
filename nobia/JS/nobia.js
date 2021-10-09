9ee6ccb7334d18dc28d16a7c411d845cd466bb7d
PabloGranados <pgranadosm1900@alumno.ipn.mx>
2021-10-08 20:45:16 -0500

    Agregamos el programa para nobiar xd

---
+++ nobia/JS/nobia.js
@@ -0,0 +1,16 @@
+function ParaCuandoDigaSi() {
+    alert('Ahora somos nobios <3');
+}
+
+function MoverBoton(){
+    width = window.innerWidth
+    height = window.innerHeight
+
+    newWidth = (Math.random() * width);
+    newHeight = (Math.random() * height);
+
+    document.getElementById('no').style.position = "absolute";
+    document.getElementById('no').style.left = newWidth + "px";
+    document.getElementById('no').style.top = newHeight + "px";
+
+}
